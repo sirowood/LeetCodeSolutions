3ee3e0d91e9d5bd99603ba4a537f34b048015e7b
Xuefeng Wu <sirowood@gmail.com>
2024-07-12 10:36:36 +0300

    easy: leetcode-27

---
+++ top_interview_150/2.27-remove_element.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/remove-element/?envType=study-plan-v2&envId=top-interview-150
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  // Fast and slow pointers
+  // Use for-loop to check the fast item, if the fast item !== val, replace it with the slow item, and increase slow pointer, because the problem doesn't care about the order.
+  // Then we can ensure the slow pointer always pointing to the most right valid item position
+
+  let slowPointer = 0;
+
+  for (let fastPointer = 0; fastPointer < nums.length; fastPointer++) {
+    if (nums[fastPointer] !== val) {
+      nums[slowPointer] = nums[fastPointer];
+      slowPointer++;
+    }
+  }
+
+  return slowPointer;
+};
